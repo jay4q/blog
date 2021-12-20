73ce8a85bcbb409c654543989fe0da7432395de3
jay4q <jay4qqqq@gmail.com>
2021-12-20 17:22:15 +0800

    upd: 🔧 调整深色模式的方法

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'media', // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
   },
