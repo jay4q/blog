0ee4274ec0e09ec252af25b28c91301a8023ec03
jay4q <jay4qqqq@gmail.com>
2021-12-20 15:18:45 +0800

    feat: 📖  补全个人和项目介绍

--- public/__post/about.md
+++ public/__post/about.md
@@ -7,8 +7,20 @@ createTime: "2021-12-20"
 updateTime: "2021-12-20"
 ---
 
-Hey👋 我是Jay4Q。目前是 [多棱镜](https://www.3lengjing.com/) 的前端主管。负责公司 [Museverse](weixin://dl/business/?t=bXuEe205Tka) 产品的架构设计和开发工作；同时，负责公司部分项目的大前端开发工作，例如 [吴文化博物馆](https://wuzhongmuseum.com)、[耳朵里的博物馆](weixin://dl/business/?t=TcNBK2ufVuq) 等等～
+Hey👋 我是Jay4Q，目前是 [多棱镜](https://www.3lengjing.com/) 的前端主管。负责公司 [Museverse](weixin://dl/business/?t=stPwTDkLdod) 产品的架构设计和开发工作；同时，负责公司部分项目的大前端开发工作，例如 [吴文化博物馆](https://wuzhongmuseum.com)、[耳朵里的博物馆](weixin://dl/business/?t=TcNBK2ufVuq) 等等～
 
 个人擅长使用最新、稳定的前后端技术栈（如 Typescript、Nodejs、Nextjs 等），实现性能优异、交互友好的应用 🚀 。配合目前最主流的 serverless 云技术，甲方 **无需支付服务器和后端开发费用** ，即可获得全栈的开发服务～
 
 更多项目，可移步至👉 [Projects](https://blog.jay4q.com/projects) 中查看
+
+## 了解更多👇
+
+### 2014-2018
+
+1. 苏州大学本科，软件工程专业
+2. 2017年(第10届)中国大学生计算机设计大赛获二等奖
+3. 2017年江苏省大学生计算机设计大赛获三等奖
+
+### 2011-2014
+
+1. 就读于江苏省锡山高级中学
---
+++ src/apis/project.ts
@@ -0,0 +1,56 @@
+export type ProjectModel = {
+  title: string
+  desc: string
+  url: string
+}
+
+export const companyData: ProjectModel[] = [
+  {
+    title: 'Museverse',
+    desc: '中小博物馆开放服务平台，助力博物馆为公众提供更便捷、更智慧的服务',
+    url: 'weixin://dl/business/?t=stPwTDkLdod',
+  },
+  {
+    title: '耳朵里的博物馆',
+    desc: '在这里你将获得有关博物馆的线上与线下服务与文博知识，让我们一起更轻松地爱上博物馆',
+    url: 'weixin://dl/business/?t=TcNBK2ufVuq',
+  },
+  {
+    title: '吴文化博物馆',
+    desc: '苏州市吴文化博物馆官方小程序',
+    url: 'weixin://dl/business/?t=YVDg5ZcBf8m',
+  },
+  {
+    title: '吴文化博物馆官网',
+    desc: '苏州市吴文化博物馆官方门户网站',
+    url: 'https://www.wuzhongmuseum.com/',
+  },
+  {
+    title: '灼烁重现',
+    desc: '上海博物馆《灼烁重现：十五世纪中期景德镇瓷器大展》互动游戏',
+    url: 'https://wm.kzl.la/shm/zscx/',
+  },
+  {
+    title: '古代官场升职记',
+    desc: '江苏教育博物馆｜古代官场升职记｜互动游戏',
+    url: 'https://spa.museshow.cn/jsjy-cxg/',
+  },
+]
+
+export const githubData: ProjectModel[] = [
+  {
+    title: 'Blitz Func',
+    desc: '腾讯云函数模板，让单个云函数具备并行处理多项业务的能力',
+    url: 'https://github.com/jay4q/blitz-func',
+  },
+  {
+    title: 'Blitz Libs',
+    desc: 'Web开发常用的UI组件、工具函数集合',
+    url: 'https://github.com/jay4q/blitz-libs',
+  },
+  {
+    title: 'Blog',
+    desc: '个人博客😁',
+    url: 'https://github.com/jay4q/blog',
+  }
+]
