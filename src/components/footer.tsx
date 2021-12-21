da324fe8b123aed39cdc606550156267fa0120e2
jay4q <jay4qqqq@gmail.com>
2021-12-21 16:41:08 +0800

    feat: 🚀 完成项目展示页

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import { FunctionComponent } from 'react'
 
 export const Footer: FunctionComponent = () => {
   return (
-    <footer className='w-full mt-8 pt-4 border-t-2 border-gray-200 dark:border-gray-800 flex-shrink-0'>
+    <footer className='w-full mt-8 pt-4 border-t border-gray-200 dark:border-gray-800 flex-shrink-0'>
       <p className='w-full text-center text-sm text-gray-400 dark:text-gray-500'>
         Powered by <a href='https://nextjs.org/' target='_blank' className='text-gray-500 dark:text-gray-400'>Nextjs</a> & 
         Deployed on <a href='https://vercel.com/' target='_blank' className='text-gray-500 dark:text-gray-400'>Vercel</a>
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,8 +1,57 @@
+import { companyData, githubData, ProjectModel } from '@/apis/project'
 import { FunctionComponent } from 'react'
+import { CursorClickIcon } from '@heroicons/react/outline'
 
+const GithubItem: FunctionComponent<{ data: ProjectModel, index: number }> = ({ data, index }) => {
+  return (
+    <li key={data.url} className='w-full md:w-[calc(100%/3-1rem)] p-1 rounded-xl transition-all origin-center hover:scale-[1.025] bg-gradient-to-b md:bg-gradient-to-r odd:from-green-400 odd:to-blue-400 even:from-blue-400 even:to-green-400'>
+      <a href={data.url} className='flex flex-col justify-between w-full h-[160px] md:h-[200px] p-4 bg-gray-50 dark:bg-gray-900 rounded-lg'>
+        <h1 className='font-medium text-lg text-gray-900 dark:text-gray-100 line-clamp-3 md:line-clamp-4 break-all'>
+          {data.title}
+        </h1>
+        <div className='flex items-center'>
+          <CursorClickIcon className='w-5 h-5 text-gray-900 dark:text-gray-100 mr-1' />
+          <span className='text-base text-gray-900 dark:text-gray-100 capitalize'>see more</span>
+        </div>
+      </a>
+    </li>
+  )
+}
+
+const WorkItem: FunctionComponent<{ data: ProjectModel, index: number }> = ({ data, index }) => {
+  return (
+    <li key={data.url} className='w-full md:w-[calc(100%/3-1rem)] p-1 rounded-xl transition-all origin-center hover:scale-[1.025] bg-gradient-to-b md:bg-gradient-to-r odd:from-blue-400 odd:to-orange-400 even:from-orange-400 even:to-blue-400'>
+      <a href={data.url} className='flex flex-col justify-between w-full h-[160px] md:h-[200px] p-4 bg-gray-50 dark:bg-gray-900 rounded-lg'>
+        <h1 className='font-medium text-lg text-gray-900 dark:text-gray-100 line-clamp-3 md:line-clamp-4 break-all'>
+          {data.title}
+        </h1>
+        <div className='flex items-center'>
+          <CursorClickIcon className='w-5 h-5 text-gray-900 dark:text-gray-100 mr-1' />
+          <span className='text-base text-gray-900 dark:text-gray-100 capitalize'>see more</span>
+        </div>
+      </a>
+    </li>
+  )
+}
+
+/**
+ * @description 项目展示页
+ */
 const Page: FunctionComponent = () => {
   return (
     <>
+      <h1 className='mt-8 mb-6 text-2xl font-bold text-gray-900 dark:text-gray-100'>🎨 开源项目</h1>
+      <ul className='w-full flex flex-wrap gap-4 md:gap-6'>
+        {
+          githubData.map((data, index) => <GithubItem key={data.url} data={data} index={index} />)
+        }
+      </ul>
+      <h1 className='mt-8 mb-6 text-2xl font-bold text-gray-900 dark:text-gray-100'>👨‍💻 工作案例</h1>
+      <ul className='w-full flex flex-wrap gap-4 md:gap-6'>
+        {
+          companyData.map((data, index) => <WorkItem key={data.url} data={data} index={index} />)
+        }
+      </ul>
     </>
   )
 }
