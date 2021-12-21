a2553ccaffb01518e0cb007941b32774280de13a
jay4q <jay4qqqq@gmail.com>
2021-12-21 10:20:59 +0800

    style: 🎨 优化页脚

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,12 @@
 import { FunctionComponent } from 'react'
-import { ExternalLinkIcon } from '@heroicons/react/outline'
 
 export const Footer: FunctionComponent = () => {
   return (
     <footer className='w-full mt-8 pt-4 border-t-2 border-gray-200 dark:border-gray-800 flex-shrink-0'>
-      <a href='https://github.com/jay4q' className='flex items-center' target='_blank'>
-        <span className='text-sm text-gray-500 mr-1'>Github</span>
-        <ExternalLinkIcon className='text-gray-500 w-4 h-4' />
-      </a>
+      <p className='w-full text-center text-sm text-gray-400 dark:text-gray-500'>
+        Powered by <a href='https://nextjs.org/' target='_blank' className='text-gray-500 dark:text-gray-400'>Nextjs</a> & 
+        Deployed on <a href='https://vercel.com/' target='_blank' className='text-gray-500 dark:text-gray-400'>Vercel</a>
+      </p>
     </footer>
   )
 }
